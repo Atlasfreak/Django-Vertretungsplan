cdcd88d07fa94f3f6159bae05e2bcdf2fef26dc9
Atlasfreak <60364953+Atlasfreak@users.noreply.github.com>
2020-04-27 23:41:11 +0200

    added cookie banner js; made functions

--- static/js/my.js
+++ static/js/my.js
@@ -7,7 +7,43 @@ function show_field(value){
     }
 };
 
+function back_to_top(){
+    let btn = $("button#back-to-top")
 
+    $(window).scroll(function() {
+        if ( $(this).scrollTop() > 300 ) {
+            btn.fadeIn();
+        } else {
+            btn.fadeOut();
+        }
+    });
+
+    btn.click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 700);
+        return false;
+    });
+
+}
+
+function cookie_banner(){
+    let keyvalue = document.cookie.match("(^|;) ?cookie_consent=([^;]*)(;|$)");
+    let cookie_consentCookie = keyvalue ? decodeURIComponent(keyvalue[2]) : null;
+
+    if (cookie_consentCookie) return;
+    else {
+        $(".cookie-consent").removeClass("hidden")
+    }
+
+    $(".cookie-consent-accept").click( function(){
+        let max_age = (365*24*60*60);
+        cookie_message = "accepted"
+        document.cookie = `cookie_consent=${encodeURIComponent(cookie_message)}; max-age=${max_age};`
+        location.reload();
+    });
+
+}
 
 $(document).ready(function() {
     // from stackoverflow
@@ -47,21 +83,8 @@ $(document).ready(function() {
     });
 
     //back to top button
-    var btn = $("button#back-to-top")
+    back_to_top();
 
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 300 ) {
-            //btn.removeClass("hidden");
-            btn.fadeIn();
-        } else {
-            btn.fadeOut();
-        }
-    });
-
-    btn.click(function() {
-        $("html, body").animate({
-            scrollTop: 0
-        }, 700);
-        return false;
-    });
+    //cookie banner
+    cookie_banner();
 });
